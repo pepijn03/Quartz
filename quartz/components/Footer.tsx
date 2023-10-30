3d60ccfb12e5c8eaf0a7ea01cf783a0ef20452a6
Pepijn van Loenhout <pepijnvanloenhout@gmail.com>
2023-10-30 22:49:57 +0100

    updated color palette, removed some links from the footer and expanded graph depth

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -16,13 +16,6 @@ export default ((opts?: Options) => {
         <p>
           Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © {year}
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
       </footer>
     )
   }
